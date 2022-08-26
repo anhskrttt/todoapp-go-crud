a11e88e704e9ea9422db894885a783d2e075db5d
anhskrttt <duonanh195@gmail.com>
2022-08-26 02:29:00 -0700

    added simple ui

--- routers/routers.go
+++ routers/routers.go
@@ -11,6 +11,23 @@ func Routers() http.Handler {
 	// Default With the Logger and Recovery middleware already attached
 	r := gin.Default()
 
+	// Load CSS file
+	r.Static("public", "public")
+
+	// Load HTML file
+	r.LoadHTMLGlob("templates/*")
+
+	// Serve static files
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(
+			http.StatusOK,
+			"index.html",
+			gin.H{
+				"status": http.StatusOK,
+			},
+		)
+	})
+
 	// More about gin Context: https://stackoverflow.com/questions/63522977/how-does-context-struct-work-in-golang-gin-framework
 	// The gin Context is a structure that contains both the http.Request and the http.Response
 	// Test API
@@ -23,7 +40,7 @@ func Routers() http.Handler {
 	r.POST("/api/task", controllers.CreateTask)
 
 	r.PUT("/api/tasks/:id", controllers.UpdateTaskById)
-	
+
 	r.DELETE("/api/tasks/:id", controllers.DeleteTaskById)
 	r.DELETE("/api/tasks", controllers.DeleteAllTasks)
 
